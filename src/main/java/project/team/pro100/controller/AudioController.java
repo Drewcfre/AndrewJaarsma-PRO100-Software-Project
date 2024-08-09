72c51fdce248fe6c8fbbac0aeb8d6a21f9aa3909
ZacharyNye <znye@student.neumont.edu>
2024-08-09 16:41:24 -0600

    made audio controller work, put everything in a base package, added a module-info so audio controller functioned, among other small fixes to make everything work correctly without breaking existing code.

--- src/main/java/project/team/pro100/controller/AudioController.java
+++ src/main/java/project/team/pro100/controller/AudioController.java
@@ -1,7 +1,9 @@
 package project.team.pro100.controller;
 
+import javafx.animation.PauseTransition;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 import project.team.pro100.RhythmApp;
 
 import java.io.File;
@@ -16,31 +18,37 @@ public class AudioController {
             throws InterruptedException {
         Media media = new Media(new File(mediaLocation).toURI().toString());
         soundValueOutput = new MediaPlayer(media);
+        soundValueOutput.setMute(true);
         playAudio = new MediaPlayer(media);
 
         //wait(delayInMilliseconds);
 
         if(hasListener) {
             soundValueOutput.setAudioSpectrumListener(((timestamp, duration, magnitudes, phases) -> {
-                for (int i = 0; i < magnitudes.length; i+=70) {
-                    if (magnitudes[i] > -59.5 && magnitudes[i] < -54) {
+                for (int i = 0; i < magnitudes.length; i+=50) {
+                    if (magnitudes[i] > -58 && magnitudes[i] < -57) {
                         RhythmApp.getList(0).add(spawn("ArrowRed", 0, 512));
                     }
-                    else if (magnitudes[i] >= -52 && magnitudes[i] < -48) {
+                    else if (magnitudes[i] >= -48 && magnitudes[i] < -44) {
                         RhythmApp.getList(1).add(spawn("ArrowGreen", 64, 576));
                     }
-                    else if (magnitudes[i] >= -46 && magnitudes[i] < -43) {
+                    else if (magnitudes[i] >= -38 && magnitudes[i] < -36) {
                         RhythmApp.getList(2).add(spawn("ArrowBlue",    192, 576));
                     }
-                    else if (magnitudes[i] >= -39 && magnitudes[i] < -38) {
+                    else if (magnitudes[i] >= -28 && magnitudes[i] < -24) {
                         RhythmApp.getList(3).add(spawn("ArrowYellow", 256, 512));
                     }
                 }
             }));
         }
 
-        soundValueOutput.setMute(true);
         soundValueOutput.play();
-        playAudio.play();
+
+        PauseTransition pause = new PauseTransition(Duration.millis(1650));
+        pause.setOnFinished(e -> {
+            playAudio.play();
+        });
+        pause.play();
+
     }
 }
