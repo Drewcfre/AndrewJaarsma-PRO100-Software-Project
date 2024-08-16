10b19650e52a1cdaec0b0a0138bd6a183e1bb239
MLazoM <aquteasever@gmail.com>
2024-08-15 18:07:30 -0600

    Update FileController.java

--- src/main/java/project/team/pro100/controller/FileController.java
+++ src/main/java/project/team/pro100/controller/FileController.java
@@ -6,8 +6,43 @@
  */
 package project.team.pro100.controller;
 
-public class FileController {
-    // This can be saved for week 2 and 3.
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
-    // This is a test commit
+
+import java.io.File;
+
+public class FileController {
+    private MediaPlayer mediaPlayer;
+
+    public MediaPlayer playAudioFromPath(String filePath) {
+        if (!isValidAudioFile(filePath)) {
+            System.out.println("Invalid audio file: " + filePath);
+            return null;
+        }
+
+        Media media = new Media(new File(filePath).toURI().toString());
+
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+
+        return mediaPlayer;
+    }
+
+    private boolean isValidAudioFile(String filePath) {
+        File file = new File(filePath);
+        return file.exists() && (filePath.endsWith(".wav") || filePath.endsWith(".mp3") || filePath.endsWith(".aac"));
+    }
+
+
+    public void changeAudio(String newFilePath) {
+        playAudioFromPath(newFilePath);
+    }
 }
+
+
+
